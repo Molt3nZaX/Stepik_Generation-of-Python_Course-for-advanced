6a19e35ad2879dfd8036d51902539bd3c87e63fd
Molt3nZaX <ishkaa803@gmail.com>
2022-10-07 12:39:28 +0400

    Generation of Py 2_2.1

--- Tusk1.py
+++ Tusk1.py
@@ -1,3 +1,9 @@
-dasfgnhmjgit
-grhjhgef
-sfsff
+from math import sqrt
+a, b = int(input()), int(input())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(sqrt(a ** 10 + b ** 10))
